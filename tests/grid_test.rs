abf80ce0ec6036bf28683bc10a39ccd083b716d0
kkew3 <kps6326@hotmail.com>
2025-09-11 18:48:57 +0800

    Add test for `isclose`

--- tests/grid_test.rs
+++ tests/grid_test.rs
@@ -1,5 +1,3 @@
-//! Run bound validity test on grid over the globe.
-
 use geo::{Destination, Geodesic, Point};
 
 fn linspace(min: f64, max: f64, num: u32) -> Vec<f64> {
@@ -7,8 +5,9 @@ fn linspace(min: f64, max: f64, num: u32) -> Vec<f64> {
     (0..num).map(move |i| min + (i as f64) * step).collect()
 }
 
+/// Run bound validity test on grid over the globe.
 #[test]
-fn grid_test() {
+fn bound_grid_test() {
     let tol = 1e-8; // Floating point error tolerance.
     let lons = linspace(-180.0, 180.0, 37);
     let lats = linspace(-90.0, 90.0, 19);
@@ -33,6 +32,53 @@ fn grid_test() {
                         dst.x(),
                         dst.y()
                     );
+                }
+            }
+        }
+    }
+}
+
+/// Run isclose correctness test on grid over the globe.
+#[cfg(feature = "geo")]
+#[test]
+fn isclose_grid_test() {
+    let tol = 1e-8; // Floating point error tolerance.
+    let lons = linspace(-180.0, 180.0, 37);
+    let lats = linspace(-90.0, 90.0, 19);
+    for lon in lons.iter().copied() {
+        for lat in lats.iter().copied() {
+            let src = Point::new(lon, lat);
+            for bearing in linspace(0.0, 360.0, 36) {
+                for log_distance in linspace(-2.0, 7.0, 50) {
+                    let distance = 10f64.powf(log_distance);
+                    let dst = Geodesic.destination(src, bearing, distance);
+                    for log_radius in linspace(-3.0, 8.0, 50) {
+                        let radius = 10f64.powf(log_radius);
+                        let close = geo_prox::isclose(
+                            src,
+                            dst,
+                            radius,
+                            &Geodesic,
+                            geo_prox::A_TOL,
+                            geo_prox::R_TOL,
+                            true,
+                        );
+                        // Assert equality only when the difference is
+                        // non-negligible.
+                        if (distance - radius).abs() > 1e-8 + 1e-5 * radius {
+                            assert_eq!(
+                                close,
+                                distance <= radius,
+                                "is_close: {} | (distance - radius): {} @ ({}, {}) ~ ({}, {})",
+                                close,
+                                distance - radius,
+                                src.x(),
+                                src.y(),
+                                dst.x(),
+                                dst.y(),
+                            );
+                        }
+                    }
                 }
             }
         }
