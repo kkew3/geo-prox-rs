2e25b1c691b87784d607b1335499be0a46c5f683
kkew3 <kps6326@hotmail.com>
2025-09-11 11:32:44 +0800

    Fix the upper bound for large chord distance case

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,13 @@
 //!
 //! ```
 //! use geo_types::Point;
+//! use geo_prox::{A_TOL, R_TOL};
 //! let p1 = Point::new(23.319941, 42.698334);
 //! let p2 = Point::new(23.319920, 42.698323);
-//! assert_eq!(geo_prox::isclose_opt(p1, p2, 15.0, None, None), Some(true));
+//! assert_eq!(
+//!     geo_prox::isclose_opt(p1, p2, 15.0, A_TOL, R_TOL, true),
+//!     Some(true)
+//! );
 //! ```
 
 #[cfg(feature = "geo")]
@@ -26,7 +30,17 @@ mod wgs84 {
     pub const E2: f64 = F * (2.0 - F);
     /// Semi-minor axis (polar radius) in meters.
     pub const B: f64 = A * (1.0 - F);
+    /// Minimum normal-section radius of curvature of the ellipsoid.
+    const RHO: f64 = B * B / A;
+    pub const RHO2: f64 = RHO + RHO;
+    /// Half-equator length.
+    pub const EQ_FRAC2: f64 = std::f64::consts::PI * A;
 }
+
+/// The default absolute tolerance, taken from numpy.
+pub const A_TOL: f64 = 1e-8;
+/// The default relative tolerance, taken from numpy.
+pub const R_TOL: f64 = 1e-5;
 
 /// The ECEF coordinate.
 #[derive(Debug, Clone, Copy)]
@@ -64,37 +78,29 @@ impl Ecef<f64> {
     }
 }
 
-/// A conservative bound on (geodesic - chord) using cubic small-distance
-/// approximation derived from the spherical case.
-const fn max_geodesic_minus_chord_bound(chord_dist: f64) -> f64 {
-    (chord_dist * chord_dist * chord_dist) / (24.0 * wgs84::B * wgs84::B)
-}
-
 /// Test if the error `err` (non-negative) is tiny comparing with `b`
 /// (non-negative).
 const fn err_is_tiny(err: f64, b: f64, a_tol: f64, r_tol: f64) -> bool {
     err <= a_tol + r_tol * b
 }
 
-/// Return a lower bound and an optional upper bound of the geodesic distance
-/// between two points (longitude, latitude) in degrees. If the lower bound is
-/// greater than `radius`, only the lower bound will be returned; otherwise,
-/// both bounds will be returned. Set `radius` to infinity if one wants to
-/// ensure the upper bound is not None.
-pub fn geodesic_distance_bound(
-    p1: Point<f64>,
-    p2: Point<f64>,
-    radius: f64,
-) -> (f64, Option<f64>) {
+/// Return the lower and upper bounds of the geodesic distance between two
+/// points (longitude, latitude) in degrees.
+pub fn geodesic_distance_bound(p1: Point<f64>, p2: Point<f64>) -> (f64, f64) {
     let ecef_p1: Ecef<_> = p1.into();
     let ecef_p2: Ecef<_> = p2.into();
-    let d_c = ecef_p1.chord_distance(ecef_p2);
 
-    if d_c > radius {
-        (d_c, None)
+    // The chord distance is obviously a lower bound.
+    let d_c = ecef_p1.chord_distance(ecef_p2);
+    // For d_c <= 2 RHO, an upper bound is the circular-arc length:
+    // 2 RHO arcsin(d_c / (2 RHO)). This already covers most d_c. Otherwise,
+    // take the half-equator length (the catch-all upper bound).
+    let ub1 = wgs84::RHO2 * (d_c / wgs84::RHO2).asin();
+    let ub2 = wgs84::EQ_FRAC2;
+    if ub1.is_nan() {
+        (d_c, ub2)
     } else {
-        let err_upper_bound = max_geodesic_minus_chord_bound(d_c);
-        (d_c, Some(d_c + err_upper_bound))
+        (d_c, ub1.min(ub2))
     }
 }
 
@@ -105,13 +111,18 @@ pub fn geodesic_distance_bound(
 /// to [`geo::GeodesicMeasure`] if `radius` is too large to make the chord
 /// distance a tight enough bound. Whether `radius` is regarded as "too
 /// large" is decided by the absolute tolerance `a_tol` and relative tolerance
-/// `r_tol`. Passing None to use the default tolerances, which are sensible for
-/// most use cases.
+/// `r_tol`. The `default` parameter decides what to return when the
+/// bounds are tight and `radius` satisfies lowerbound <= radius < upperbound.
 ///
 /// # Accepted coordinate value
 ///
 /// - longitude: -180.0..=180.0, negative for west, positive for east.
 /// - latitude: -90.0..=90.0, negative for south, positive for north.
+///
+/// # Notes on tolerance parameters
+///
+/// One may check [`geo_prox::A_TOL`] and [`geo_prox::R_TOL`] for sensible
+/// defaults.
 ///
 /// # Notes on coordinate type
 ///
@@ -123,34 +134,26 @@ pub fn isclose(
     p2: Point<f64>,
     radius: f64,
     fallback_dist: &GeodesicMeasure,
-    a_tol: Option<f64>,
-    r_tol: Option<f64>,
+    a_tol: f64,
+    r_tol: f64,
+    default: bool,
 ) -> bool {
-    let (lb, ub_opt) = geodesic_distance_bound(p1, p2, radius);
+    let (lb, ub) = geodesic_distance_bound(p1, p2);
 
-    match ub_opt {
-        None => {
-            // If the lower bound already exeeds `radius`, `p1` and `p2` are
-            // definitely not close.
-            false
-        }
-        Some(ub) => {
-            // The absolute tolerance, default taken from numpy.
-            let a_tol = a_tol.unwrap_or(1e-8);
-            // The relative tolerance, default taken from numpy.
-            let r_tol = r_tol.unwrap_or(1e-5);
-
-            if !err_is_tiny(ub - lb, ub, a_tol, r_tol) {
-                // The bound is too loose. We have to resort to the exact
-                // distance measure.
-                fallback_dist.distance(p1, p2) <= radius
-            } else {
-                // Trade off a little bit precision for speed, as the bound is
-                // tight enough, assuming that `a_tol` and `r_tol` values are
-                // sensible.
-                true
-            }
-        }
+    if lb > radius {
+        // If the lower bound already exeeds `radius`, `p1` and `p2` are
+        // definitely not close.
+        false
+    } else if ub <= radius {
+        true
+    } else if !err_is_tiny(ub - lb, ub, a_tol, r_tol) {
+        // The bound is too loose. We have to resort to the exact
+        // distance measure.
+        fallback_dist.distance(p1, p2) <= radius
+    } else {
+        // Since the bounds are tight enough, it's safe to follow the
+        // default decision. This branch should not be reached very often.
+        default
     }
 }
 
@@ -160,13 +163,19 @@ pub fn isclose(
 /// scale of a few hundred meters of `radius`, the error is tiny. Return None
 /// if `radius` is too large to make the chord distance a tight enough bound.
 /// Whether `radius` is regarded as "too large" is decided by the absolute
-/// tolerance `a_tol` and relative tolerance `r_tol`. Passing None to use the
-/// default tolerances, which are sensible for most use cases.
+/// tolerance `a_tol` and relative tolerance `r_tol`. The `default`
+/// parameter decides what to return when the bounds are tight and `radius`
+/// satisfies lowerbound <= radius < upperbound.
 ///
 /// # Accepted coordinate value
 ///
 /// - longitude: -180.0..=180.0, negative for west, positive for east.
 /// - latitude: -90.0..=90.0, negative for south, positive for north.
+///
+/// # Notes on tolerance parameters
+///
+/// One may check [`geo_prox::A_TOL`] and [`geo_prox::R_TOL`] for sensible
+/// defaults.
 ///
 /// # Notes on coordinate type
 ///
@@ -176,33 +185,25 @@ pub fn isclose_opt(
     p1: Point<f64>,
     p2: Point<f64>,
     radius: f64,
-    a_tol: Option<f64>,
-    r_tol: Option<f64>,
+    a_tol: f64,
+    r_tol: f64,
+    default: bool,
 ) -> Option<bool> {
-    let (lb, ub_opt) = geodesic_distance_bound(p1, p2, radius);
+    let (lb, ub) = geodesic_distance_bound(p1, p2);
 
-    match ub_opt {
-        None => {
-            // If the lower bound already exeeds `radius`, `p1` and `p2` are
-            // definitely not close.
-            Some(false)
-        }
-        Some(ub) => {
-            // The absolute tolerance, default taken from numpy.
-            let a_tol = a_tol.unwrap_or(1e-8);
-            // The relative tolerance, default taken from numpy.
-            let r_tol = r_tol.unwrap_or(1e-5);
-
-            if !err_is_tiny(ub - lb, ub, a_tol, r_tol) {
-                // The bound is too loose. We choose to abstain.
-                None
-            } else {
-                // Trade off a little bit precision for speed, as the bound is
-                // tight enough, assuming that `a_tol` and `r_tol` values are
-                // sensible.
-                Some(true)
-            }
-        }
+    if lb > radius {
+        // If the lower bound already exeeds `radius`, `p1` and `p2` are
+        // definitely not close.
+        Some(false)
+    } else if ub <= radius {
+        Some(true)
+    } else if !err_is_tiny(ub - lb, ub, a_tol, r_tol) {
+        // The bound is too loose. We choose to abstain.
+        None
+    } else {
+        // Since the bounds are tight enough, it's safe to follow the
+        // default decision. This branch should not be reached very often.
+        Some(default)
     }
 }
 
@@ -214,6 +215,9 @@ mod tests {
     fn test_isclose() {
         let p1 = Point::new(23.319941, 42.698334);
         let p2 = Point::new(23.319920, 42.698323);
-        assert_eq!(super::isclose_opt(p1, p2, 15.0, None, None), Some(true));
+        assert_eq!(
+            super::isclose_opt(p1, p2, 15.0, super::A_TOL, super::R_TOL, true),
+            Some(true)
+        );
     }
 }
